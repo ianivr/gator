59c07cc67faeda910bcc7cbd745571c52c6e9e51
ianivr <ianivrubinstein@gmail.com>
2026-06-18 04:28:51 +0000

    Add SQL queries for creating and inserting feeds into the database

---
+++ sql/schema/002_feeds.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE TABLE feeds (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    UNIQUE (url),
+    user_id UUID NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE feeds;
