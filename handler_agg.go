59ac9da2252791827fe19cd2c6554ef52ea4c46d
ianivr <ianivrubinstein@gmail.com>
2026-06-21 18:19:17 +0000

    Implement feed aggregation with configurable intervals and enhance feed fetching logic

--- handler_agg.go
+++ handler_agg.go
@@ -3,14 +3,47 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 func handleAggregate(s *state, cmd command) error {
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
-	if err != nil {
-		return fmt.Errorf("failed to fetch feed: %w", err)
+	if len(cmd.args) < 1 {
+		return fmt.Errorf("agg command requires a time between requests")
 	}
 
-	fmt.Printf("Feed: %v\n", feed)
+	timeBetweenRequests, err := time.ParseDuration(cmd.args[0])
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Collecting feeds every %v\n", timeBetweenRequests)
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	defer ticker.Stop()
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) error {
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return err
+	}
+
+	feed, err = s.db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		return err
+	}
+
+	feedFetched, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		return err
+	}
+
+	for _, item := range feedFetched.Channel.Item {
+		fmt.Printf("- %v\n", item.Title)
+	}
+
 	return nil
 }
